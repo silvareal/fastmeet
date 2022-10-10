6b2e8c4b575bf8a50d85f8839849b1034d3b4a32
sylvernus akubo <akubosylvernus@gmail.com>
2022-10-10 11:22:27 +0100

    CHORES: generate in lowercase

--- client/src/utils/VideoUtils.tsx
+++ client/src/utils/VideoUtils.tsx
@@ -5,7 +5,7 @@
  */
 export function getRandomCharacters(length: number): string {
   let result: string = "";
-  let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let characters = "abcdefghijklmnopqrstuvwxyz0123456789";
   let charactersLength = characters.length;
   for (let i = 0; i < length; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));

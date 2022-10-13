515209394d7d9317d2c3fdf3549df7fa0d26f1fa
sylvernus akubo <akubosylvernus@gmail.com>
2022-10-13 09:49:39 +0100

    CHORES: fallback stun server :unicorn:

--- server/dist/services/external.service.js
+++ server/dist/services/external.service.js
@@ -10,28 +10,61 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getTwilioTurnServer = void 0;
+const GlobalUtils_1 = require("../helpers/GlobalUtils");
 const twillioAuthToken = process.env.TWILIO_AUTH_TOKEN;
 const twillioAccountSID = process.env.TWILIO_ACCOUNT_SID;
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const twilio = require("twilio")(twillioAccountSID, twillioAuthToken);
+const twilio = require('twilio')(twillioAccountSID, twillioAuthToken);
+const log = new GlobalUtils_1.Logger();
+/**
+ * https://www.metered.ca/tools/openrelay/#-stun-server-usage
+ */
+const fallbackTurnServer = {
+    iceServers: [
+        {
+            urls: 'stun:openrelay.metered.ca:80',
+        },
+        {
+            urls: 'turn:openrelay.metered.ca:80',
+            username: 'openrelayproject',
+            credential: 'openrelayproject',
+        },
+        {
+            urls: 'turn:openrelay.metered.ca:443',
+            username: 'openrelayproject',
+            credential: 'openrelayproject',
+        },
+        {
+            urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+            username: 'openrelayproject',
+            credential: 'openrelayproject',
+        },
+    ],
+};
+/**
+ * Generate twilio turn server
+ * @returns object
+ */
 function getTwilioTurnServer() {
     return __awaiter(this, void 0, void 0, function* () {
         let response = {};
         if (!!twillioAuthToken === false || !!twillioAccountSID === false) {
             const error = new Error();
-            error.message = "Twilio Secret keys required";
+            const message = 'Twilio Secret keys required';
+            log.error(message);
+            error.message = message;
             throw error;
         }
         response = yield twilio.tokens
             .create()
             .then((token) => {
+            log.info('Twilio Turn server generated', token);
             return token;
         })
             .catch((err) => {
-            console.log(err);
-            const error = new Error();
-            error.message = `${err}`;
-            throw error;
+            // if twilio turn server false fallback to default
+            log.error(err, fallbackTurnServer);
+            return fallbackTurnServer;
         });
         return response;
     });
--- server/src/services/external.service.ts
+++ server/src/services/external.service.ts
@@ -1,27 +1,62 @@
+import {Logger} from '../helpers/GlobalUtils';
+
 const twillioAuthToken = process.env.TWILIO_AUTH_TOKEN;
 const twillioAccountSID = process.env.TWILIO_ACCOUNT_SID;
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const twilio = require("twilio")(twillioAccountSID, twillioAuthToken);
+const twilio = require ('twilio') (twillioAccountSID, twillioAuthToken);
 
-export async function getTwilioTurnServer() {
+const log = new Logger ();
+/**
+ * https://www.metered.ca/tools/openrelay/#-stun-server-usage
+ */
+const fallbackTurnServer = {
+  iceServers: [
+    {
+      urls: 'stun:openrelay.metered.ca:80',
+    },
+    {
+      urls: 'turn:openrelay.metered.ca:80',
+      username: 'openrelayproject',
+      credential: 'openrelayproject',
+    },
+    {
+      urls: 'turn:openrelay.metered.ca:443',
+      username: 'openrelayproject',
+      credential: 'openrelayproject',
+    },
+    {
+      urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+      username: 'openrelayproject',
+      credential: 'openrelayproject',
+    },
+  ],
+};
+
+/**
+ * Generate twilio turn server
+ * @returns object
+ */
+export async function getTwilioTurnServer () {
   let response = {};
   if (!!twillioAuthToken === false || !!twillioAccountSID === false) {
-    const error = new Error();
-    error.message = "Twilio Secret keys required";
+    const error = new Error ();
+    const message = 'Twilio Secret keys required';
+    log.error (message);
+    error.message = message;
     throw error;
   }
 
   response = await twilio.tokens
-    .create()
-    .then((token: any) => {
+    .create ()
+    .then ((token: never) => {
+      log.info ('Twilio Turn server generated', token);
       return token;
     })
-    .catch((err: Error) => {
-      console.log(err);
-      const error = new Error();
-      error.message = `${err}`;
-      throw error;
+    .catch ((err: Error) => {
+      // if twilio turn server false fallback to default
+      log.error (err, fallbackTurnServer);
+      return fallbackTurnServer;
     });
 
   return response;

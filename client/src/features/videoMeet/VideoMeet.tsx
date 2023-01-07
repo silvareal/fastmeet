eebfeb7fa425161c8371956e62de9cd63d21174e
JoshX <joshx.dev@gmail.com>
2023-01-07 20:32:53 +0100

    remove unused `var`

--- client/src/features/videoMeet/VideoMeet.tsx
+++ client/src/features/videoMeet/VideoMeet.tsx
@@ -1,4 +1,4 @@
-import { Fab, Icon, Input, Tooltip, Typography } from "@mui/material";
+import { Fab, Icon, Tooltip, Typography } from "@mui/material";
 import React, { useMemo } from "react";
 import { Icon as Iconify } from "@iconify/react";
 import { format } from "date-fns";
